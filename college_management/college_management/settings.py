09dad1c65a92a69b08b884d66f27b46371937a7f
Ashutosh Vaish <38212544+ashutoshvaish@users.noreply.github.com>
2025-07-12 21:04:02 +0530

    Update settings.py

--- college_management/college_management/settings.py
+++ college_management/college_management/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'college_management.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
